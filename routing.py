a4218858c0090b32851dca2f8d82b95178331e16
Kevin Sandoval <ks777@scarletmail.rutgers.edu>
2017-11-01 00:38:51 -0400

    updating/slowing testing
    didn`t have much time, so i worked a bit on the project, mainly in the "testing" file

--- routing.py
+++ routing.py
@@ -4,45 +4,42 @@
 #1. Intro page to set up user preference (so car make/model/year)
 #2. Page to display result
 
-
-
 from flask import Flask, url_for, request, render_template
 from app import app
-import quandl
 import math
-import requests
+import untangle
 
-resp = requests.get("http://www.fueleconomy.gov/feg/ws/rest/fuelprices")
-print(resp)
+response = untangle.parse("http://www.fueleconomy.gov/ws/rest/fuelprices")
+print(response.fuelPrices.regular.cdata)
 
-#def cashback(x):
-  #  z = x * .03
-  #  x = x - z
-  #  print(x)
-
-
+#Credit card gives 3% cashback for gas purchases.
+def cashback(x):
+   z = x * .03
+   x = x - z
+   print(x)
 
 #first page/where we collect data from the user
-#@app.route('/')
-#def itstime():
-#    return render_template('SENDINTHEFINISHER.html')
+@app.route('/')
+def itstime():
+    return render_template('SENDINTHEFINISHER.html')
 
 
 #second page where we display outputs to the user
-#@app.route('/Had this idea for a while, but wanted to give it a shot today!', methods = ['GET','POST'])
-#def itsover():
-  #  if request.method == 'POST':
+@app.route('/Had this idea for a while, but wanted to give it a shot today!', methods = ['GET','POST'])
+def itsover():
+    if request.method == 'POST':
     #space reserved for calling the database. Most of the work comes here
     #vehicle will be the mock variable called to reference the database output
     #assuming you can even get the value. for now, put a placement value
-    #    gas = 2.50
-     #   gas2 = data + .05   #low point credit card
-      #  gas3 = data + .10  #high point credit card
-       # cash = data * vehicle
-        #credit_low = gas2 * vehicle
-       # credit_high = gas3 * vehicle
-       # saving = cashback(credit_low)
-       # saving = cashback(credit_high)
+        gas2 = response + [.05]   #low point credit card
+        gas3 = response + .10  #high point credit card
+        cash = response * 16 #16 is the combined mpg of the 2002 trailblazer 4WD
+        credit_low = gas2 * 16
+        credit_high = gas3 * 16
+        saving_low = cashback(credit_low)
+        saving_high = cashback(credit_high)
+        print(saving_low)
+        print(saving_high)
         #would need to add car model when database is finished
         #if credit_high > cash:
           #  int returned = credit_high - cash
--- trying.py
+++ trying.py
@@ -1,8 +1,30 @@
 #yes its the price of gas currently
 #IGNORE HTML PROCESS ATM, AND FOCUS ON BASE CODEWORK
 
+import math
 import untangle
 
 response = untangle.parse("http://www.fueleconomy.gov/ws/rest/fuelprices")
-print(response.fuelPrices.regular.cdata)
+x = float(response.fuelPrices.regular.cdata)
+
+def cashback(x):
+   z = x * .03
+   x = x - z
+   return(x)
+
+
+gas2 = x + .05   #low point credit card
+print(gas2)
+gas3 = x + .10  #high point credit card
+print(gas3)
+cash = x * 16 #16 is the combined mpg of the 2002 trailblazer 4WD
+print(cash)
+credit_low = gas2 * 16
+print(credit_low)
+#credit_high = gas3 * 16
+#print(credit_high)
+saving_low = cashback(credit_low)
+#saving_high = cashback(credit_high)
+print(saving_low)
+#print(saving_high)
 
