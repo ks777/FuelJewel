190da74d0ae5757af09cb296d566a3eb610bd622
Kevin Sandoval <ks777@scarletmail.rutgers.edu>
2017-11-03 01:37:07 -0400

    slowly working on idea/creation
    trying to finalize what i want to ultimately do with this project.

--- routing.py
+++ routing.py
@@ -28,41 +28,41 @@ def itstime():
 @app.route('/Had this idea for a while, but wanted to give it a shot today!', methods = ['GET','POST'])
 def itsover():
     if request.method == 'POST':
-    #space reserved for calling the database. Most of the work comes here
-    #vehicle will be the mock variable called to reference the database output
-    #assuming you can even get the value. for now, put a placement value
-        gas2 = x + .05   #low point credit card
-        print(gas2)
-        gas3 = x + .10  #high point credit card
-        print(gas3)
-        cash = x * 16 #16 is the combined mpg of the 2002 trailblazer 4WD
-        print(cash)
-        credit_low = gas2 * 16
-        print(credit_low)
-        #credit_high = gas3 * 16
-        #print(credit_high)
-        saving_low = cashback(credit_low)
-        #saving_high = cashback(credit_high)
-        print(saving_low)
-        #print(saving_high)
-        
-        #would need to add car model when database is finished
-        #if credit_high > cash:
-          #  int returned = credit_high - cash
-         #   return render_template('Ialwaysgottasendinthefinisher1.html', credit_high = credit_high, cash = cash )
-        #elif credit_high < cash:
-         #   int returned = cash - credit_high
-          #  return render_template('Ialwaysgottasendinthefinisher1.html', credit_high = credit_high, cash = cash )
-        #elif credit_low > cash:
-         #   int returned = credit_low - cash
-          #  return render_template('Ialwaysgottasendinthefinisher2.html', credit_low = credit_low, cash = cash )
-        #elif credit_low < cash:
-         #   int returned = cash - credit_low
-          #  return render_template('Ialwaysgottasendinthefinisher2.html', credit_low = credit_low, cash = cash )
-        #else:
-        #    return render_template('Ialwaysgottasendinthefinisher3.html')  
-    #API_response = requests.get("https://www.quandl.com/api/v3/datasets/FRED/GASALLCOVW")
-    #gas_price = response.data[0]
-    #else:
-     #   shutdown_server()
-     #   return('Thank you for using our product!')
+       #do i need to calculate the highest credit cost
+       #remember, i`m attempting to determine the best deals, so i need to assume the cheapest value
+       #which appears to be a .06 credit increase
+       gas2 = x + .06   #low point credit card
+       print("Cost of gas using credit card, at its lowest price: $",gas2)
+       gas3 = x + .10  #high point credit card/normal cost
+       print("Cost of gas using credit card, at its highest price: $",gas3)
+       cash = x * 16 #16 is the combined mpg of the 2002 trailblazer 4WD
+       print("This is the amount you would pay for a full tank, in cash: $",cash)
+       credit_low = gas2 * 16
+       print("This is the amount you would pay for a full tank, in the lowest price in credit card:$",credit_low)
+       credit_high = gas3 * 16
+       print("This is the amount you would pay for a full tank, in the highest price in credit card:$",credit_high)
+       saving_low = cashback(credit_low)
+       saving_high = cashback(credit_high)
+       print("This is the end result of the lowest credit spending, assuming cashback: $",saving_low)       #total i spend on a full tank of gas using credit minus the $3 cashback
+       print("This is the end result of the highest credit spending, assuming cashback: $",saving_high)
+
+       if cash > saving_low:
+          savings = cash - saving_low
+          savings = round(savings,2)
+          print("You save more with cash! How much more? Well you save: $",savings)
+      elif cash > saving_high:
+         savings = cash - saving_high
+         savings = round(savings,2)
+         print("You save more by using cash at this price. You save: $", savings)
+      elif saving_high > cash:
+         savings = saving_high - cash
+         savings = round(savings,2)
+         print("You save more by using your credit card at this price. You save: $", savings)
+      elif saving_low > cash:
+         savings = saving_low - cash
+         savings = round(savings,2)
+         print("You save more by using your credit card at this price. You save: $", savings)
+      else:
+         print("DO WHATEVER, ITS THE SAME PRICE REALLY!")
+
+
--- trying.py
+++ trying.py
@@ -10,23 +10,56 @@ x = float(response.fuelPrices.regular.cdata)
 def cashback(x):
    z = x * .03
    x = x - z
-   return(x)
+   return(round(x,2))
 
 #do i need to calculate the highest credit cost
 #remember, i`m attempting to determine the best deals, so i need to assume the cheapest value
 #which appears to be a .06 credit increase
-gas2 = x + .05   #low point credit card
-print(gas2)
-gas3 = x + .10  #high point credit card
-print(gas3)
+
+cgas = input("Enter the price, in decimal form, of purchasing gas via credit card: ")
+pgas = input("Enter the percentage of gas you need to fill your tank: ")
+
+#gas2 = x + .06   #low point credit card
+#print("Cost of gas using credit card, at its lowest price: $",gas2)
+#gas3 = x + .10  #high point credit card/normal cost
+#print("Cost of gas using credit card, at its highest price: $",gas3)
+
 cash = x * 16 #16 is the combined mpg of the 2002 trailblazer 4WD
-print(cash)
+print("This is the amount you would pay for a full tank, in cash: $",cash)
 credit_low = gas2 * 16
-print(credit_low)
-#credit_high = gas3 * 16
-#print(credit_high)
+print("This is the amount you would pay for a full tank, in the lowest price in credit card:$",credit_low)
+credit_high = gas3 * 16
+print("This is the amount you would pay for a full tank, in the highest price in credit card:$",credit_high)
 saving_low = cashback(credit_low)
-#saving_high = cashback(credit_high)
-print(saving_low)       #total i spend on a full tank of gas using credit minus the $3 cashback
-#print(saving_high)
+saving_high = cashback(credit_high)
+print("This is the end result of the lowest credit spending, assuming cashback: $",saving_low)       #total i spend on a full tank of gas using credit minus the $3 cashback
+print("This is the end result of the highest credit spending, assuming cashback: $",saving_high)
+if cash > saving_low:
+   savings = cash - saving_low
+   savings = round(savings,2)
+   print("You save more with cash! How much more? Well you save: $",savings)
+elif cash > saving_high:
+   savings = cash - saving_high
+   savings = round(savings,2)
+   print("You save more by using cash at this price. You save: $", savings)
+elif saving_high > cash:
+   savings = saving_high - cash
+   savings = round(savings,2)
+   print("You save more by using your credit card at this price. You save: $", savings)
+elif saving_low > cash:
+   savings = saving_low - cash
+   savings = round(savings,2)
+   print("You save more by using your credit card at this price. You save: $", savings)
+else:
+   print("DO WHATEVER, ITS THE SAME PRICE REALLY!")
+
+
+#MAIN QUESTION: DOES MY CAULCATION CHANGE IF I DON`T SPEND A FULL TANK?
+#LIKE 75%/50%/25%
+#it does not. reducing it all equals the same answer.
+#the only thing that can change would be the credit value
+#so if you do % change and credit increase change, then the values would matter a lot.
+   #so: variable to initial credit value  and percentage change
+   #so then you only need 2 values.
+
 
